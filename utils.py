c96fafb0d007d4cefd88cf94f31b264a44b8abbf
tobybreckon <toby.breckon@cranfield.ac.uk>
2018-10-23 12:34:43 +0100

    add patch sampling for training

--- utils.py
+++ utils.py
@@ -13,11 +13,14 @@ import os
 import numpy as np
 import cv2
 import params
+import random
 
 ################################################################################
-# global flag to facilitate output of additional info per stage/function
+# global flags to facilitate output of additional info per stage/function
 
 show_additional_process_information = True;
+show_images_as_they_are_loaded = True;
+show_images_as_they_are_sampled = True;
 
 ################################################################################
 
@@ -76,10 +79,6 @@ def read_all_images(path):
 
         if show_additional_process_information:
             print("loading file - ", image_path);
-
-        # to debug image loading uncomment lines below
-        # cv2.imshow("loaded", img)
-        # cv2.waitKey(200)
 
         images.append(img)
     return images
@@ -169,9 +168,54 @@ class ImageData(object):
 
 ################################################################################
 
-# add images from a specified path to the dataset, adding the appropriate class/type name
+# generates a set of random sample patches from a given image of a specified size
 
-def load_image_path(path, class_name, imgs_data):
+def generate_patches(img, sample_patches_to_generate=0, patch_size=(64,128)):
+
+    patches = [];
+
+    # if no patches specifed just return original image
+
+    if (sample_patches_to_generate == 0):
+        return [img];
+
+    # otherwise generate N sub patches
+
+    else:
+
+        # get all heights and widths
+
+        img_height, img_width, _ = img.shape;
+        patch_height = patch_size[1];
+        patch_width = patch_size[0];
+
+        # iterate to find up to N patches (0 -> N-1)
+
+        for patch_count in range(sample_patches_to_generate):
+
+            # randomly select a patch, ensuring we stay inside the image
+
+            patch_start_h =  random.randint(0, (img_height - patch_height));
+            patch_start_w =  random.randint(0, (img_width - patch_width));
+
+            # add this patch to the list of patches
+
+            patch = img[patch_start_h:patch_start_h + patch_height, patch_start_w:patch_start_w + patch_width]
+
+            if (show_images_as_they_are_sampled):
+                cv2.imshow("patch", patch);
+                cv2.waitKey(5);
+
+            patches.insert(patch_count, patch);
+
+        return patches;
+
+################################################################################
+
+# add images from a specified path to the dataset, adding the appropriate class/type name
+# and optionally adding up to N samples of a specified size
+
+def load_image_path(path, class_name, imgs_data, samples=0, patch_size=(64,128)):
 
     # read all images at location
 
@@ -180,12 +224,25 @@ def load_image_path(path, class_name, imgs_data):
     img_count = len(imgs_data)
     for img in imgs:
 
-        # add each image to the data set
+        if (show_images_as_they_are_loaded):
+            cv2.imshow("example", img);
+            cv2.waitKey(5);
 
-        img_data = ImageData(img)
-        img_data.set_class(class_name)
-        imgs_data.insert(img_count, img_data)
-        img_count += 1
+        # generate up to N sample patches for each sample image
+        # if zero samples is specified then generate_patches just returns
+        # the original image (unchanged, unsampled) as [img]
+
+        for img_patch in generate_patches(img, samples, patch_size):
+
+            if show_additional_process_information:
+                print("path: ", path, "class_name: ", class_name, "patch: ", img_count)
+
+            # add each image patch to the data set
+
+            img_data = ImageData(img_patch)
+            img_data.set_class(class_name)
+            imgs_data.insert(img_count, img_data)
+            img_count += 1
 
     return imgs_data
 
@@ -193,11 +250,14 @@ def load_image_path(path, class_name, imgs_data):
 
 # load image data from specified paths
 
-def load_images(paths, class_names):
+def load_images(paths, class_names, sample_set_sizes, patch_size=(64,128)):
     imgs_data = []  # type: list[ImageData]
 
-    for path, class_name in zip(paths, class_names):
-        load_image_path(path, class_name, imgs_data)
+    # for each specified path and corresponding class_name and required number
+    # of samples - add them to the data set
+
+    for path, class_name, sample_count in zip(paths, class_names, sample_set_sizes):
+        load_image_path(path, class_name, imgs_data, sample_count, patch_size)
 
     return imgs_data
 
